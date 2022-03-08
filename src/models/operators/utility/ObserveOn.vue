17d017b20f337e84d530c19ee73926d8f4812419
howlwindy <howlwindy@gmail.com>
2022-03-08 21:33:51 +0800

    feat(operators): utility
    observeOn

--- src/models/operators/utility/ObserveOn.vue
+++ src/models/operators/utility/ObserveOn.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'observeOn<T>(scheduler: SchedulerLike, delay?: number): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>observeOn</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
