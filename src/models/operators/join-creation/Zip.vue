d337aac69124ccf1b52a9f9819ffb1f3704e23cf
howlwindy <howlwindy@gmail.com>
2022-03-08 21:08:02 +0800

    feat(operators): join creation
    zip

--- src/models/operators/join-creation/Zip.vue
+++ src/models/operators/join-creation/Zip.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'zip<A extends readonly unknown[]>(sources: [...ObservableInputTuple<A>]): Observable<A>',
+  'zip<A extends readonly unknown[], R>(sources: [...ObservableInputTuple<A>], resultSelector: (...values: A) => R): Observable<R>',
+  'zip<A extends readonly unknown[]>(...sources: [...ObservableInputTuple<A>]): Observable<A>',
+  'zip<A extends readonly unknown[], R>(...sourcesAndResultSelector: [...ObservableInputTuple<A>, (...values: A) => R]): Observable<R>'
+]
+</script>
 <template>
   <h4><code>zip</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
