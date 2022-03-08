42670adddedf3f49e136d51304308fa0d6a59a62
howlwindy <howlwindy@gmail.com>
2022-03-08 20:56:51 +0800

    feat(operators): creation
    timer

--- src/models/operators/creation/Timer.vue
+++ src/models/operators/creation/Timer.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'timer(due: number | Date, scheduler?: SchedulerLike): Observable<0>',
+  'timer(startDue: number | Date, intervalDuration: number, scheduler?: SchedulerLike): Observable<number>'
+]
+</script>
 <template>
   <h4><code>timer</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
