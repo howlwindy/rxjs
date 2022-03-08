23804ef45bffb40655433a5c76c04490aa9ddbe6
howlwindy <howlwindy@gmail.com>
2022-03-08 20:55:42 +0800

    feat(operators): creation
    throwError

--- src/models/operators/creation/ThrowError.vue
+++ src/models/operators/creation/ThrowError.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['throwError(errorFactory: () => any): Observable<never>']
+</script>
 <template>
   <h4><code>throwError</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
