b9445f1c41d9b5acb28fafd2fd317e4dde1fee76
howlwindy <howlwindy@gmail.com>
2022-03-08 21:04:34 +0800

    feat(operators): join creation
    merge

--- src/models/operators/join-creation/Merge.vue
+++ src/models/operators/join-creation/Merge.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'merge<A extends readonly unknown[]>(...sources: [...ObservableInputTuple<A>]): Observable<A[number]>',
+  'merge<A extends readonly unknown[]>(...sourcesAndConcurrency: [...ObservableInputTuple<A>, number?]): Observable<A[number]>'
+]
+</script>
 <template>
   <h4><code>merge</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
