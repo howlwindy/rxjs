b3c6ae29886fe6d4b0fe30f8db47f52867b2d3a3
howlwindy <howlwindy@gmail.com>
2022-03-08 21:40:58 +0800

    feat(operators): utility
    timeoutWith

--- src/models/operators/utility/TimeoutWith.vue
+++ src/models/operators/utility/TimeoutWith.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'timeoutWith<T, R>(dueBy: Date, switchTo: ObservableInput<R>, scheduler?: SchedulerLike): OperatorFunction<T, T | R>'
+]
+</script>
 <template>
   <h4><code>timeoutWith</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
