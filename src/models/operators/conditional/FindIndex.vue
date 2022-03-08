e52740e1b264e3d32ba122747d4edd6e29081797
howlwindy <howlwindy@gmail.com>
2022-03-08 21:22:45 +0800

    feat(operators): conditionsl
    findIndex

--- src/models/operators/conditional/FindIndex.vue
+++ src/models/operators/conditional/FindIndex.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'findIndex<T>(predicate: BooleanConstructor): OperatorFunction<T, T extends Falsy ? -1 : number>',
+  'findIndex<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean): OperatorFunction<T, number>'
+]
+</script>
 <template>
   <h4><code>findIndex</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
