8a9683d2d6b4a260556aca525ef74f647cb8ec00
howlwindy <howlwindy@gmail.com>
2022-03-08 21:28:56 +0800

    feat(operators): utility
    delayWhen

--- src/models/operators/utility/DelayWhen.vue
+++ src/models/operators/utility/DelayWhen.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'delayWhen<T>(delayDurationSelector: (value: T, index: number) => Observable<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>delayWhen</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
