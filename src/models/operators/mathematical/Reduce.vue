35b4584b0e0f62a42924cc5d1528b3d5ff7b138d
howlwindy <howlwindy@gmail.com>
2022-03-08 21:16:42 +0800

    feat(operators): mathematical
    reduce

--- src/models/operators/mathematical/Reduce.vue
+++ src/models/operators/mathematical/Reduce.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'reduce<V, A = V>(accumulator: (acc: A | V, value: V, index: number) => A): OperatorFunction<V, V | A>',
+  'reduce<V, A>(accumulator: (acc: A, value: V, index: number) => A, seed: A): OperatorFunction<V, A>',
+  'reduce<V, A, S = A>(accumulator: (acc: A | S, value: V, index: number) => A, seed: S): OperatorFunction<V, A>'
+]
+</script>
 <template>
   <h4><code>reduce</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
