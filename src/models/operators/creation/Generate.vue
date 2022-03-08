689d5e1a3db8c86d49300b0524c811eac217ef50
howlwindy <howlwindy@gmail.com>
2022-03-08 20:48:28 +0800

    feat(operators): creation
    generate

--- src/models/operators/creation/Generate.vue
+++ src/models/operators/creation/Generate.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'generate<S>(options: GenerateBaseOptions<S>): Observable<S>',
+  'generate<T, S>(options: GenerateOptions<T, S>): Observable<T>'
+]
+</script>
 <template>
   <h4><code>generate</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
