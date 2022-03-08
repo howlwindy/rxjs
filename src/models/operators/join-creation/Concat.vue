df29882ea7bd10b815e2076d6f0aa1572e94eaa3
howlwindy <howlwindy@gmail.com>
2022-03-08 21:01:35 +0800

    feat(operators): join creation
    concat

--- src/models/operators/join-creation/Concat.vue
+++ src/models/operators/join-creation/Concat.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'concat<T extends readonly unknown[]>(...inputs: [...ObservableInputTuple<T>]): Observable<T[number]>',
+  'concat<T extends readonly unknown[]>(...inputsAndScheduler: [...ObservableInputTuple<T>, SchedulerLike]): Observable<T[number]>'
+]
+</script>
 <template>
   <h4><code>concat</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
