4e6e5ce7f4f207c4e674b4eafbcb37ab0dc9df8c
howlwindy <howlwindy@gmail.com>
2022-03-08 21:14:56 +0800

    feat(operators): mathematical
    max

--- src/models/operators/mathematical/Max.vue
+++ src/models/operators/mathematical/Max.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'max<T>(comparer?: (x: T, y: T) => number): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>max</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
