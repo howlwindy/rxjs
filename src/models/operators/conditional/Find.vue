c3e2520123d65495a43cce026f9cf127915218d1
howlwindy <howlwindy@gmail.com>
2022-03-08 21:21:35 +0800

    feat(operators): conditional
    find

--- src/models/operators/conditional/Find.vue
+++ src/models/operators/conditional/Find.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'find<T>(predicate: BooleanConstructor): OperatorFunction<T, TruthyTypesOf<T>>',
+  'find<T, S extends T>(predicate: (value: T, index: number, source: Observable<T>) => value is S): OperatorFunction<T, S | undefined>',
+  'find<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean): OperatorFunction<T, T | undefined>'
+]
+</script>
 <template>
   <h4><code>find</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
