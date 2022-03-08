013852dfc04d9d76e9a449dc192631aa112edf18
howlwindy <howlwindy@gmail.com>
2022-03-08 21:06:51 +0800

    feat(operators): join creation
    race

--- src/models/operators/join-creation/Race.vue
+++ src/models/operators/join-creation/Race.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'race<T extends readonly unknown[]>(inputs: [...ObservableInputTuple<T>]): Observable<T[number]>',
+  'race<T extends readonly unknown[]>(...inputs: [...ObservableInputTuple<T>]): Observable<T[number]>'
+]
+</script>
 <template>
   <h4><code>race</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
