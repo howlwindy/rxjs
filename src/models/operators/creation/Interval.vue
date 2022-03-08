11713b8bd3e02d24031fec059e72dc5495d102b0
howlwindy <howlwindy@gmail.com>
2022-03-08 20:50:17 +0800

    feat(operators): creation
    interval

--- src/models/operators/creation/Interval.vue
+++ src/models/operators/creation/Interval.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'interval(period?: number, scheduler?: SchedulerLike): Observable<number>'
+]
+</script>
 <template>
   <h4><code>interval</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
