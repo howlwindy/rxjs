249c4d670588eced2006ba008e2a46532bf46fce
howlwindy <howlwindy@gmail.com>
2022-03-08 21:45:13 +0800

    feat(operators): error handling
    retry

--- src/models/operators/error-handing/Retry.vue
+++ src/models/operators/error-handing/Retry.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'retry<T>(count?: number): MonoTypeOperatorFunction<T>',
+  'retry<T>(config: RetryConfig): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>retry</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
