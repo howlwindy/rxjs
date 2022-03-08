61e8bf44e2d4c8b49fc3008cae1224846b5184b5
howlwindy <howlwindy@gmail.com>
2022-03-08 21:44:14 +0800

    feat(operators): error handling
    catchError

--- src/models/operators/error-handing/CatchError.vue
+++ src/models/operators/error-handing/CatchError.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'catchError<T, O extends ObservableInput<any>>(selector: (err: any, caught: Observable<T>) => O): OperatorFunction<T, T | ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>catchError</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
