5d2cb3baf824eace846a280216d8b2274537ebdc
howlwindy <howlwindy@gmail.com>
2022-03-08 20:39:57 +0800

    feat(operators): creation
    defer

--- src/models/operators/creation/Defer.vue
+++ src/models/operators/creation/Defer.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'defer<R extends ObservableInput<any>>(observableFactory: () => R): Observable<ObservedValueOf<R>>'
+]
+</script>
 <template>
   <h4><code>defer</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
