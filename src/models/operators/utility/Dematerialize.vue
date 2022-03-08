2df41546afb89d2dcb39060d856c78de06b05bcb
howlwindy <howlwindy@gmail.com>
2022-03-08 21:31:16 +0800

    feat(operators): utility
    dematerialize

--- src/models/operators/utility/Dematerialize.vue
+++ src/models/operators/utility/Dematerialize.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'dematerialize<N extends ObservableNotification<any>>(): OperatorFunction<N, ValueFromNotification<N>>'
+]
+</script>
 <template>
   <h4><code>dematerialize</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
