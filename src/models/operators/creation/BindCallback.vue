39c75ba88eafcea291627126e1633c92f597ea46
howlwindy <howlwindy@gmail.com>
2022-03-08 20:36:16 +0800

    feat(operators): creation
    bindCallback

--- src/models/operators/creation/BindCallback.vue
+++ src/models/operators/creation/BindCallback.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'bindCallback(callbackFunc: (...args: any[]) => void, resultSelector: (...args: any[]) => any, scheduler?: SchedulerLike): (...args: any[]) => Observable<any>',
+  'bindCallback<A extends readonly unknown[], R extends readonly unknown[]>(callbackFunc: (...args: [...A, (...res: R) => void]) => void, schedulerLike?: SchedulerLike): (...arg: A) => Observable<R extends [] ? void : R extends [any] ? R[0] : R>'
+]
+</script>
 <template>
   <h4><code>bindCallback</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
