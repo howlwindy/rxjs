e29c9711add813270f3882984e49c164fe5e56c2
howlwindy <howlwindy@gmail.com>
2022-03-08 20:46:34 +0800

    feat(operators): creation
    fromEventPattern

--- src/models/operators/creation/FromEventPattern.vue
+++ src/models/operators/creation/FromEventPattern.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'fromEventPattern<T>(addHandler: (handler: NodeEventHandler) => any, removeHandler?: (handler: NodeEventHandler, signal?: any) => void): Observable<T>',
+  'fromEventPattern<T>(addHandler: (handler: NodeEventHandler) => any, removeHandler?: (handler: NodeEventHandler, signal?: any) => void, resultSelector?: (...args: any[]) => T): Observable<T>'
+]
+</script>
 <template>
   <h4><code>fromEventPattern</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
