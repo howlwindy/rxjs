88f994579c04fe8b44bf4fe1a1ce6cb52892977d
howlwindy <howlwindy@gmail.com>
2022-03-08 21:42:12 +0800

    feat(operators): utility
    timestamp

--- src/models/operators/utility/Timestamp.vue
+++ src/models/operators/utility/Timestamp.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'timestamp<T>(timestampProvider?: TimestampProvider): OperatorFunction<T, Timestamp<T>>'
+]
+</script>
 <template>
   <h4><code>timestamp</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
