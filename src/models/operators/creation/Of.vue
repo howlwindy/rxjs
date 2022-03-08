2f0285525e5e5669c49796906f8226a44ea66d76
howlwindy <howlwindy@gmail.com>
2022-03-08 20:51:26 +0800

    feat(operators): creation
    of

--- src/models/operators/creation/Of.vue
+++ src/models/operators/creation/Of.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'of(value: null): Observable<null>',
+  'of(value: undefined): Observable<undefined>',
+  'of(): Observable<never>',
+  'of<T>(value: T): Observable<T>',
+  'of<A extends readonly unknown[]>(...values: A): Observable<ValueFromArray<A>>'
+]
+</script>
 <template>
   <h4><code>of</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
