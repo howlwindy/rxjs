82d8ab6a71aaba01346abbc3efce59560db19f98
howlwindy <howlwindy@gmail.com>
2022-03-08 21:13:56 +0800

    feat(operators): mathematical
    count

--- src/models/operators/mathematical/Count.vue
+++ src/models/operators/mathematical/Count.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'count<T>(predicate?: (value: T, index: number) => boolean): OperatorFunction<T, number>'
+]
+</script>
 <template>
   <h4><code>count</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
