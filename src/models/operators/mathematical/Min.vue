cdc73fef8da3c1b37ed2d5ae93971e7bef574dc6
howlwindy <howlwindy@gmail.com>
2022-03-08 21:15:37 +0800

    feat(operators): mathematical
    min

--- src/models/operators/mathematical/Min.vue
+++ src/models/operators/mathematical/Min.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'min<T>(comparer?: (x: T, y: T) => number): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>min</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
