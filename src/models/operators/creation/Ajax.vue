dd2caff9d8efa2d8637338bf45cd36bc8b7ec6da
howlwindy <howlwindy@gmail.com>
2022-03-08 20:33:23 +0800

    feat(operators): creation
    ajax

--- src/models/operators/creation/Ajax.vue
+++ src/models/operators/creation/Ajax.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'ajax<T>(config: AjaxConfig): Observable<AjaxResponse<T>>',
+  'ajax<T>(url: string): Observable<AjaxResponse<T>>'
+]
+</script>
 <template>
   <h4><code>ajax</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
