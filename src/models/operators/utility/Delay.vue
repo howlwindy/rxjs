e35418dd176f0497635a6113db2f0ee4e1a55ccf
howlwindy <howlwindy@gmail.com>
2022-03-08 21:27:47 +0800

    feat(operators): utility
    delay

--- src/models/operators/utility/Delay.vue
+++ src/models/operators/utility/Delay.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'delay<T>(due: number | Date, scheduler?: SchedulerLike): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>delay</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
