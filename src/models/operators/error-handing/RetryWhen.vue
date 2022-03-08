c18dba61afef1022c1529007543a812c7e5fb9a0
howlwindy <howlwindy@gmail.com>
2022-03-08 21:46:04 +0800

    feat(operators): error handling
    retryWhen

--- src/models/operators/error-handing/RetryWhen.vue
+++ src/models/operators/error-handing/RetryWhen.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'retryWhen<T>(notifier: (errors: Observable<any>) => Observable<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>retryWhen</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
