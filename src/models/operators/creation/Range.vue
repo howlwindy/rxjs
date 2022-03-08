1db041b7e307699e2fed2652eee639853f8db249
howlwindy <howlwindy@gmail.com>
2022-03-08 20:52:26 +0800

    feat(operators): creation
    range

--- src/models/operators/creation/Range.vue
+++ src/models/operators/creation/Range.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'range(start: number, count?: number): Observable<number>',
+  'range(start: number, count: number | undefined, scheduler: SchedulerLike): Observable<number>'
+]
+</script>
 <template>
   <h4><code>range</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
