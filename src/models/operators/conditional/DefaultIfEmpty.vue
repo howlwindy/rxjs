79655c028e9cea5b4dfee415cdef83a21f624145
howlwindy <howlwindy@gmail.com>
2022-03-08 21:18:58 +0800

    feat(operators): conditional
    defaultIfEmpty

--- src/models/operators/conditional/DefaultIfEmpty.vue
+++ src/models/operators/conditional/DefaultIfEmpty.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'defaultIfEmpty<T, R>(defaultValue: R): OperatorFunction<T, T | R>'
+]
+</script>
 <template>
-  <h4><code>defaultEmpty</code></h4>
+  <h4><code>defaultIfEmpty</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
