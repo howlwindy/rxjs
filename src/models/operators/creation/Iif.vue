ddfd4b4b53c9403eb50397b2a9fec970122e479e
howlwindy <howlwindy@gmail.com>
2022-03-08 20:49:30 +0800

    feat(operators): creation
    iif

--- src/models/operators/creation/Iif.vue
+++ src/models/operators/creation/Iif.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'iif<T, F>(condition: () => boolean, trueResult: ObservableInput<T>, falseResult: ObservableInput<F>): Observable<T | F>'
+]
+</script>
 <template>
   <h4><code>iif</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
