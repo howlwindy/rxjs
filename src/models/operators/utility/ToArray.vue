2dce80abc2e319c9c4bac76ee485dc0a20b7bbbb
howlwindy <howlwindy@gmail.com>
2022-03-08 21:42:57 +0800

    feat(operators): utility
    toArray

--- src/models/operators/utility/ToArray.vue
+++ src/models/operators/utility/ToArray.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['toArray<T>(): OperatorFunction<T, T[]>']
+</script>
 <template>
   <h4><code>toArray</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
