f872073963d695fdad0c46339540152df0fd6e7f
howlwindy <howlwindy@gmail.com>
2022-03-08 21:32:43 +0800

    feat(operators): utility
    materialize

--- src/models/operators/utility/Materialize.vue
+++ src/models/operators/utility/Materialize.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'materialize<T>(): OperatorFunction<T, Notification<T> & ObservableNotification<T>>'
+]
+</script>
 <template>
   <h4><code>materialize</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
