ad2408f8f0af1ef78237472c3ab4b3fee2c68816
howlwindy <howlwindy@gmail.com>
2022-03-08 21:20:02 +0800

    feat(operators): conditional
    every

--- src/models/operators/conditional/Every.vue
+++ src/models/operators/conditional/Every.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'every<T>(predicate: BooleanConstructor): OperatorFunction<T, Exclude<T, Falsy> extends never ? false : boolean>',
+  'every<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean): OperatorFunction<T, boolean>'
+]
+</script>
 <template>
   <h4><code>every</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
