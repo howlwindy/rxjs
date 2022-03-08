f8e879439c6628311967d622a7129f0eff41e20d
howlwindy <howlwindy@gmail.com>
2022-03-08 21:35:45 +0800

    feat(operators): utility
    timeInterval

--- src/models/operators/utility/TimeInterval.vue
+++ src/models/operators/utility/TimeInterval.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'timeInterval<T>(scheduler?: SchedulerLike): OperatorFunction<T, TimeInterval<T>>'
+]
+</script>
 <template>
   <h4><code>timeInterval</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
