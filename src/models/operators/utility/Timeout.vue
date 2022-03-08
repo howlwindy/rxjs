c5861f5431bedbadaf5070866e3817a16127a288
howlwindy <howlwindy@gmail.com>
2022-03-08 21:38:36 +0800

    feat(operators): utility
    timeout

--- src/models/operators/utility/Timeout.vue
+++ src/models/operators/utility/Timeout.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'timeout<T, O extends ObservableInput<unknown>, M = unknown>(config: TimeoutConfig<T, O, M> & {with: (info: TimeoutInfo<T, M>) => O',
+  `timeout<T, M = unknown>(config: Omit<TimeoutConfig<T, any, M>, 'with'>): OperatorFunction<T, T>`,
+  'timeout<T>(first: Date, scheduler?: SchedulerLike): MonoTypeOperatorFunction<T>',
+  'timeout<T>(each: number, scheduler?: SchedulerLike): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>timeout</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
