26ce08adb4c2d54fae4b45ab6ce6b4193104fd29
howlwindy <howlwindy@gmail.com>
2022-03-08 21:26:49 +0800

    feat(operators): utility
    tap

--- src/models/operators/utility/Tap.vue
+++ src/models/operators/utility/Tap.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'tap<T>(observer?: Partial<TapObserver<T>>): MonoTypeOperatorFunction<T>',
+  'tap<T>(next: (value: T) => void): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>tap</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
