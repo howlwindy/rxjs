342935f9bfd7c17f940f7f39532358d8414657c8
howlwindy <howlwindy@gmail.com>
2022-03-08 21:23:52 +0800

    feat(operators): conditional
    isEmpty

--- src/models/operators/conditional/IsEmpty.vue
+++ src/models/operators/conditional/IsEmpty.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['isEmpty<T>(): OperatorFunction<T, boolean>']
+</script>
 <template>
   <h4><code>isEmpty</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
