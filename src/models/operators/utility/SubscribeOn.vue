f59f88383ee8abb1dabd00bb4d8d179fbede3bc6
howlwindy <howlwindy@gmail.com>
2022-03-08 21:34:42 +0800

    feat(operators): utility
    subscribeOn

--- src/models/operators/utility/SubscribeOn.vue
+++ src/models/operators/utility/SubscribeOn.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'subscribeOn<T>(scheduler: SchedulerLike, delay?: number): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>subscribeOn</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
