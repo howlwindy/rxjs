dfd8321a13f5b5d8baaac170d9c0d9588a99c110
howlwindy <howlwindy@gmail.com>
2022-03-09 21:29:08 +0800

    feat(operators): filtering
    distinctUntilKeyChanged

--- src/models/operators/filtering/DistinctUntilKeyChanged.vue
+++ src/models/operators/filtering/DistinctUntilKeyChanged.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'distinctUntilKeyChanged<T>(key: keyof T): MonoTypeOperatorFunction<T>',
+  'distinctUntilKeyChanged<T, K extends keyof T>(key: K, compare: (x: T[K], y: T[K]) => boolean): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>distinctUntilKeyChanged</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
