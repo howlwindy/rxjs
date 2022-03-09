e16a68c99c792351a24ce8f79966401318920545
howlwindy <howlwindy@gmail.com>
2022-03-09 21:51:42 +0800

    feat(operators): transformation
    mergeScan

--- src/models/operators/transformation/MergeScan.vue
+++ src/models/operators/transformation/MergeScan.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'mergeScan<T, R>(accumulator: (acc: R, value: T, index: number) => ObservableInput<R>, seed: R, concurrent?: number): OperatorFunction<T, R>'
+]
+</script>
 <template>
   <h4><code>mergeScan</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
