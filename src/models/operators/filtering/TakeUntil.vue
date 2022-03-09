7400866a4c70104f255741509bd74d817353f604
howlwindy <howlwindy@gmail.com>
2022-03-09 21:13:20 +0800

    feat(operators): filtering
    takeUntil

--- src/models/operators/filtering/TakeUntil.vue
+++ src/models/operators/filtering/TakeUntil.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'takeUntil<T>(notifier: ObservableInput<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>takeUntil</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
