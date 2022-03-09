9fe3e332498266f12939b81eca21ee01ef608610
howlwindy <howlwindy@gmail.com>
2022-03-09 21:21:32 +0800

    feat(operators): filtering
    sampleTime

--- src/models/operators/filtering/SampleTime.vue
+++ src/models/operators/filtering/SampleTime.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'sampleTime<T>(period: number, scheduler?: SchedulerLike): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>sampleTime</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
