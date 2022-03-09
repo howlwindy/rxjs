597270a10408c491326b6878200f970303de06fc
howlwindy <howlwindy@gmail.com>
2022-03-09 21:11:49 +0800

    feat(operators): filtering
    takeWhile

--- src/models/operators/filtering/TakeWhile.vue
+++ src/models/operators/filtering/TakeWhile.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'takeWhile<T>(predicate: BooleanConstructor, inclusive: true): MonoTypeOperatorFunction<T>',
+  'takeWhile<T>(predicate: BooleanConstructor, inclusive: false): OperatorFunction<T, TruthyTypesOf<T>>',
+  'takeWhile<T>(predicate: BooleanConstructor): OperatorFunction<T, TruthyTypesOf<T>>',
+  'takeWhile<T, S extends T>(predicate: (value: T, index: number) => value is S): OperatorFunction<T, S>',
+  'takeWhile<T, S extends T>(predicate: (value: T, index: number) => value is S, inclusive: false): OperatorFunction<T, S>',
+  'takeWhile<T>(predicate: (value: T, index: number) => boolean, inclusive?: boolean): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>takeWhile</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
