a387c9ac439a7549efc3e018e51ca1192261d990
howlwindy <howlwindy@gmail.com>
2022-03-09 21:39:13 +0800

    feat(operators): transformation
    bufferTime

--- src/models/operators/transformation/BufferTime.vue
+++ src/models/operators/transformation/BufferTime.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'bufferTime<T>(bufferTimeSpan: number, scheduler?: SchedulerLike): OperatorFunction<T, T[]>',
+  'bufferTime<T>(bufferTimeSpan: number, bufferCreationInterval: number | null | undefined, scheduler?: SchedulerLike): OperatorFunction<T, T[]>',
+  'bufferTime<T>(bufferTimeSpan: number, bufferCreationInterval: number | null | undefined, maxBufferSize: number, scheduler?: SchedulerLike): OperatorFunction<T, T[]>'
+]
+</script>
 <template>
   <h4><code>bufferTime</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
