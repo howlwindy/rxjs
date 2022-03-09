9004a1d377267acb56ffcff53f30268ca205001d
howlwindy <howlwindy@gmail.com>
2022-03-09 21:22:24 +0800

    feat(operators): filtering
    sample

--- src/models/operators/filtering/Sample.vue
+++ src/models/operators/filtering/Sample.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'sample<T>(notifier: Observable<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>sample</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
