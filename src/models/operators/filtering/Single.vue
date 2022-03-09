1bce92089d52dd434e16e6799bb6d3f82984238c
howlwindy <howlwindy@gmail.com>
2022-03-09 21:20:34 +0800

    feat(operators): filtering
    single

--- src/models/operators/filtering/Single.vue
+++ src/models/operators/filtering/Single.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'single<T>(predicate: BooleanConstructor): OperatorFunction<T, TruthyTypesOf<T>>',
+  'single<T>(predicate?: (value: T, index: number, source: Observable<T>) => boolean): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>single</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
