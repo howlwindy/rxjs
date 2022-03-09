60873d6281c63793e89bcd64a7da2180ddc29f62
howlwindy <howlwindy@gmail.com>
2022-03-09 21:46:37 +0800

    feat(operators): transformation
    switchScan

--- src/models/operators/transformation/SwitchScan.vue
+++ src/models/operators/transformation/SwitchScan.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'switchScan<T, R, O extends ObservableInput<any>>(accumulator: (acc: R, value: T, index: number) => O, seed: R): OperatorFunction<T, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>switchScan</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
