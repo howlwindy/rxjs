32a31f4fefc813e0d56d00277b65dcc9435751ea
howlwindy <howlwindy@gmail.com>
2022-03-09 20:46:30 +0800

    feat(operators): join
    combineLatestAll

--- src/models/operators/join/CombineLatestAll.vue
+++ src/models/operators/join/CombineLatestAll.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'combineLatestAll<T>(): OperatorFunction<ObservableInput<T>, T[]>',
+  'combineLatestAll<T>(): OperatorFunction<any, T[]>',
+  'combineLatestAll<T, R>(project: (...values: T[]) => R): OperatorFunction<ObservableInput<T>, R>',
+  'combineLatestAll<R>(project: (...values: Array<any>) => R): OperatorFunction<any, R>'
+]
+</script>
 <template>
-  <h4><code>combineLatestAll</code></h4>
+  <h4><code>combineLateatAll</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
