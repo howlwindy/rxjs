9057d2708032ea2798e46a565a10576b22b29c7b
howlwindy <howlwindy@gmail.com>
2022-03-09 21:19:25 +0800

    feat(operators): filtering
    skip

--- src/models/operators/filtering/Skip.vue
+++ src/models/operators/filtering/Skip.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['skip<T>(count: number): MonoTypeOperatorFunction<T>']
+</script>
 <template>
   <h4><code>skip</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
