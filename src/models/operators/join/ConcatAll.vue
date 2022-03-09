5e105d3f7205bdd351eb909d54d8a768a4d095c9
howlwindy <howlwindy@gmail.com>
2022-03-09 20:49:43 +0800

    feat(operators): join
    concatAll

--- src/models/operators/join/ConcatAll.vue
+++ src/models/operators/join/ConcatAll.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'concatAll<O extends ObservableInput<any>>(): OperatorFunction<O, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>concatAll</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
