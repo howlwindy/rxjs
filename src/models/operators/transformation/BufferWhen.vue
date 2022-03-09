8dc2be3f8217b1fadeb5eefcebb12566f3e7d5c6
howlwindy <howlwindy@gmail.com>
2022-03-09 21:41:04 +0800

    feat(operators): transformation
    bufferWhen

--- src/models/operators/transformation/BufferWhen.vue
+++ src/models/operators/transformation/BufferWhen.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'bufferWhen<T>(closingSelector: () => ObservableInput<any>): OperatorFunction<T, T[]>'
+]
+</script>
 <template>
   <h4><code>bufferWhen</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
