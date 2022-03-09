9b9ae22a22e7d3e4414ffedf022e4c57e854545f
howlwindy <howlwindy@gmail.com>
2022-03-09 21:05:40 +0800

    feat(operators): filtering
    debounce

--- src/models/operators/filtering/Debounce.vue
+++ src/models/operators/filtering/Debounce.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'debounce<T>(durationSelector: (value: T) => ObservableInput<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>debounce</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
