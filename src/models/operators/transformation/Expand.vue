879ffe6935e74b3f1c0842c5dbc325359cab4176
howlwindy <howlwindy@gmail.com>
2022-03-09 21:57:44 +0800

    feat(operators): transformation
    expand

--- src/models/operators/transformation/Expand.vue
+++ src/models/operators/transformation/Expand.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'expand<T, O extends ObservableInput<unknown>>(project: (value: T, index: number) => O, concurrent?: number, scheduler?: SchedulerLike): OperatorFunction<T, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>expand</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
