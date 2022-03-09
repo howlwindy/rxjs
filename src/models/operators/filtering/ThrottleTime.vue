a45f06e202b581f2cf478e840715fb1835dc8c6d
howlwindy <howlwindy@gmail.com>
2022-03-09 21:08:47 +0800

    feat(operators): filtering
    throttleTime

--- src/models/operators/filtering/ThrottleTime.vue
+++ src/models/operators/filtering/ThrottleTime.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  `throttleTime<T>(duration: number, scheduler?: SchedulerLike, config?: ThrottleConfig).ThrottleConfig): MonoTypeOperatorFunction<T>`
+]
+</script>
 <template>
   <h4><code>throttleTime</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
