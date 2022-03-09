a1a1e2f54406a6c801c69a17eba1c5135f1865dc
howlwindy <howlwindy@gmail.com>
2022-03-09 21:38:14 +0800

    feat(operators): transformation
    bufferCount

--- src/models/operators/transformation/BufferCount.vue
+++ src/models/operators/transformation/BufferCount.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'bufferCount<T>(bufferSize: number, startBufferEvery?: number | null): OperatorFunction<T, T[]>'
+]
+</script>
 <template>
   <h4><code>bufferCount</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
