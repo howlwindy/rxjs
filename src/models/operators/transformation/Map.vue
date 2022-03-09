2c61b28d3afc2b22bd97d66dcc76326a3ccd6bf6
howlwindy <howlwindy@gmail.com>
2022-03-09 21:55:31 +0800

    feat(operators): transformation
    map

--- src/models/operators/transformation/Map.vue
+++ src/models/operators/transformation/Map.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'map<T, R>(project: (value: T, index: number) => R): OperatorFunction<T, R>'
+]
+</script>
 <template>
   <h4><code>map</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
