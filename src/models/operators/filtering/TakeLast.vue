e030e3ce26d000efc39de36b55a400a154224173
howlwindy <howlwindy@gmail.com>
2022-03-09 21:14:32 +0800

    feat(operators): filtering
    takeLast

--- src/models/operators/filtering/TakeLast.vue
+++ src/models/operators/filtering/TakeLast.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'takeLast<T>(count: number): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>takeLast</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
