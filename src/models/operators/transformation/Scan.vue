a5f2d393c869a5ea6e49195daa6da44b4b4ebd1a
howlwindy <howlwindy@gmail.com>
2022-03-09 21:49:49 +0800

    feat(operators): transformation
    scan

--- src/models/operators/transformation/Scan.vue
+++ src/models/operators/transformation/Scan.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'scan<V, A = V>(accumulator: (acc: A | V, value: V, index: number) => A): OperatorFunction<V, V | A>',
+  'scan<V, A>(accumulator: (acc: A, value: V, index: number) => A, seed: A): OperatorFunction<V, A>',
+  'scan<V, A, S>(accumulator: (acc: A | S, value: V, index: number) => A, seed: S): OperatorFunction<V, A>'
+]
+</script>
 <template>
   <h4><code>scan</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
