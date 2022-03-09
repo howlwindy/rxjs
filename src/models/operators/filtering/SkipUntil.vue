1e49d0ced004b528dfe0b958b9cf3b25e8cc0e2f
howlwindy <howlwindy@gmail.com>
2022-03-09 21:17:54 +0800

    feat(operators): filtering
    skipUntil

--- src/models/operators/filtering/SkipUntil.vue
+++ src/models/operators/filtering/SkipUntil.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'skipUntil<T>(notifier: Observable<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>skipUntil</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
