508aa6e3f9c46e479c2c439fd13427d2604bf710
howlwindy <howlwindy@gmail.com>
2022-03-09 20:51:28 +0800

    feat(operators): join
    mergeAll

--- src/models/operators/join/MergeAll.vue
+++ src/models/operators/join/MergeAll.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'mergeAll<O extends ObservableInput<any>>(concurrent?: number): OperatorFunction<O, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>mergeAll</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
