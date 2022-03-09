6fe4cc34dbc5966e27abe2dcbcf8f461a9227987
howlwindy <howlwindy@gmail.com>
2022-03-09 21:16:57 +0800

    feat(operators): filtering
    skipWhile

--- src/models/operators/filtering/SkipWhile.vue
+++ src/models/operators/filtering/SkipWhile.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'skipWhile<T>(predicate: BooleanConstructor): OperatorFunction<T, Extract<T, Falsy> extends never ? never : T>',
+  'skipWhile<T>(predicate: (value: T, index: number) => true): OperatorFunction<T, never>',
+  'skipWhile<T>(predicate: (value: T, index: number) => boolean): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>skipWhile</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
