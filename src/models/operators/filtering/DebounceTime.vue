a4ddee0f38b95704c20085ff5e62aa99bc7af67b
howlwindy <howlwindy@gmail.com>
2022-03-09 21:32:28 +0800

    feat(operators): filtering
    debounceTime

--- src/models/operators/filtering/DebounceTime.vue
+++ src/models/operators/filtering/DebounceTime.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'debounceTime<T>(dueTime: number, scheduler?: SchedulerLike): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>debounceTime</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
