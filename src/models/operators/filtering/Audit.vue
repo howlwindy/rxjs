992969da156d78d2a2de68e346e990e0a51dccbd
howlwindy <howlwindy@gmail.com>
2022-03-09 21:02:08 +0800

    feat(operators): filtering
    audit

--- src/models/operators/filtering/Audit.vue
+++ src/models/operators/filtering/Audit.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'audit<T>(durationSelector: (value: T) => ObservableInput<any>): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>audit</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
