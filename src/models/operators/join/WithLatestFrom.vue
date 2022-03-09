c5642bf9f721d64afe0dc1c946fb2462974b2433
howlwindy <howlwindy@gmail.com>
2022-03-09 20:56:32 +0800

    feat(operators): join
    withLatestFrom

--- src/models/operators/join/WithLatestFrom.vue
+++ src/models/operators/join/WithLatestFrom.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'withLatestFrom<T, O extends unknown[]>(...inputs: [...ObservableInputTuple<O>]): OperatorFunction<T, [T, ...O]>',
+  'withLatestFrom<T, O extends unknown[], R>(...inputs: [...ObservableInputTuple<O>, (...value: [T, ...O]) => R]): OperatorFunction<T, R>'
+]
+</script>
 <template>
   <h4><code>withLatestFrom</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
