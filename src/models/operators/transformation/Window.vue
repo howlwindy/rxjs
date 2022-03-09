79761615c0e021d0b014146ba695286704f34546
howlwindy <howlwindy@gmail.com>
2022-03-09 21:45:38 +0800

    feat(operators): transformation
    window

--- src/models/operators/transformation/Window.vue
+++ src/models/operators/transformation/Window.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'window<T>(windowBoundaries: Observable<any>): OperatorFunction<T, Observable<T>>'
+]
+</script>
 <template>
   <h4><code>window</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
