87ebaee87ea72a7057769e4107419c88e9de9e3e
howlwindy <howlwindy@gmail.com>
2022-03-09 21:30:37 +0800

    feat(operators): filtering
    distinctUntilChanged

--- src/models/operators/filtering/DistinctUntilChanged.vue
+++ src/models/operators/filtering/DistinctUntilChanged.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'distinctUntilChanged<T>(comparator?: (previous: T, current: T) => boolean): MonoTypeOperatorFunction<T>',
+  'distinctUntilChanged<T, K>(comparator: (previous: K, current: K) => boolean, keySelector: (value: T) => K): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>distinctUntilChanged</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
