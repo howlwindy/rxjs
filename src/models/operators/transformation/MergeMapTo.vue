803813d3667dc9b560496bacef5a029d7f6dbdd4
howlwindy <howlwindy@gmail.com>
2022-03-09 21:52:45 +0800

    feat(operators): transformation
    mergeMapTo

--- src/models/operators/transformation/MergeMapTo.vue
+++ src/models/operators/transformation/MergeMapTo.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'mergeMapTo<O extends ObservableInput<unknown>>(innerObservable: O, concurrent?: number): OperatorFunction<unknown, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>mergeMapTo</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
