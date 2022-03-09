1d717ac0d06c12af5b71b19b3d131be8ced23b08
howlwindy <howlwindy@gmail.com>
2022-03-09 21:53:39 +0800

    feat(operators): transformation
    mergeMap

--- src/models/operators/transformation/MergeMap.vue
+++ src/models/operators/transformation/MergeMap.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'mergeMap<T, O extends ObservableInput<any>>(project: (value: T, index: number) => O, concurrent?: number): OperatorFunction<T, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>mergeMap</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
