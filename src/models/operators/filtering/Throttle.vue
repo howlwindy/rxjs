028f449a901856944e0685013bd900e8819c422e
howlwindy <howlwindy@gmail.com>
2022-03-09 21:10:17 +0800

    feat(operators): filtering
    throttle

--- src/models/operators/filtering/Throttle.vue
+++ src/models/operators/filtering/Throttle.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'throttle<T>(durationSelector: (value: T) => ObservableInput<any>, config?: ThrottleConfig): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
-  <h4><code>Throttle</code></h4>
+  <h4><code>throttle</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
