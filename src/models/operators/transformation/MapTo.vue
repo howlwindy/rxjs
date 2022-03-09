c1872be2d0c4c1725243085acb6465ad501c3ddc
howlwindy <howlwindy@gmail.com>
2022-03-09 21:54:44 +0800

    feat(operators): transformation
    mapTo

--- src/models/operators/transformation/MapTo.vue
+++ src/models/operators/transformation/MapTo.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['mapTo<R>(value: R): OperatorFunction<unknown, R>']
+</script>
 <template>
   <h4><code>mapTo</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
