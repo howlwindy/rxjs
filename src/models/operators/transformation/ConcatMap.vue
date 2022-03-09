32db3d37f0b7c90d1d041cb39cc413845b39f589
howlwindy <howlwindy@gmail.com>
2022-03-09 22:00:21 +0800

    feat(operators): transformation
    concatMap

--- src/models/operators/transformation/ConcatMap.vue
+++ src/models/operators/transformation/ConcatMap.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'concatMap<T, O extends ObservableInput<any>>(project: (value: T, index: number) => O): OperatorFunction<T, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>concatMap</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
