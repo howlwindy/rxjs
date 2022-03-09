27a61972fc32166ea9f0da08301c3158b4f3b969
howlwindy <howlwindy@gmail.com>
2022-03-09 20:53:22 +0800

    feat(operators): join
    switchAll

--- src/models/operators/join/SwitchAll.vue
+++ src/models/operators/join/SwitchAll.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'switchAll<O extends ObservableInput<any>>(): OperatorFunction<O, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>switchAll</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
