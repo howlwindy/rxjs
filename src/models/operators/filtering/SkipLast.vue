06bd834d7a5f90be44774e6caf1497205dd2d5aa
howlwindy <howlwindy@gmail.com>
2022-03-09 21:18:44 +0800

    feat(operators): filtering
    skipLast

--- src/models/operators/filtering/SkipLast.vue
+++ src/models/operators/filtering/SkipLast.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'skipLast<T>(skipCount: number): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>skipLast</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
