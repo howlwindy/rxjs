7409e3b6aa28a8d5bea387ba53a966f80b47ff30
howlwindy <howlwindy@gmail.com>
2022-03-09 21:50:47 +0800

    feat(operators): transformation
    pairwise

--- src/models/operators/transformation/Pairwise.vue
+++ src/models/operators/transformation/Pairwise.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['pairwise<T>(): OperatorFunction<T, [T, T]>']
+</script>
 <template>
   <h4><code>pairwise</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
