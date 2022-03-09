976086d04309e2b529d9ece8c663e038243e417c
howlwindy <howlwindy@gmail.com>
2022-03-09 21:42:54 +0800

    feat(operators): transformation
    windowToggle

--- src/models/operators/transformation/WindowToggle.vue
+++ src/models/operators/transformation/WindowToggle.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'windowToggle<T, O>(openings: ObservableInput<O>, closingSelector: (openValue: O) => ObservableInput<any>): OperatorFunction<T, Observable<T>>'
+]
+</script>
 <template>
   <h4><code>windowToggle</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
