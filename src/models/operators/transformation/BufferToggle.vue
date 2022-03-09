ae800a3d6394bbf5224618707170c9d788ef46df
howlwindy <howlwindy@gmail.com>
2022-03-09 21:40:13 +0800

    feat(operators): transformation
    bufferToggle

--- src/models/operators/transformation/BufferToggle.vue
+++ src/models/operators/transformation/BufferToggle.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'bufferToggle<T, O>(openings: ObservableInput<O>, closingSelector: (value: O) => ObservableInput<any>): OperatorFunction<T, T[]>'
+]
+</script>
 <template>
   <h4><code>bufferToggle</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
