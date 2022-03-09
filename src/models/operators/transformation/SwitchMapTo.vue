44c048d054a8f7c76ccc2ae92aabd46eaa77ee22
howlwindy <howlwindy@gmail.com>
2022-03-09 21:47:40 +0800

    feat(operators): transformation
    switchMapTo

--- src/models/operators/transformation/SwitchMapTo.vue
+++ src/models/operators/transformation/SwitchMapTo.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'switchMapTo<O extends ObservableInput<unknown>>(observable: O): OperatorFunction<unknown, ObservedValueOf<O>>'
+]
+</script>
 <template>
   <h4><code>switchMapTo</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
