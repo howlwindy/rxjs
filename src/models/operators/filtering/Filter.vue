2e8881568077fd3233f900a241b89569022f73e2
howlwindy <howlwindy@gmail.com>
2022-03-09 21:26:31 +0800

    feat(operators): filtering
    filter

--- src/models/operators/filtering/Filter.vue
+++ src/models/operators/filtering/Filter.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'filter<T, S extends T>(predicate: (value: T, index: number) => value is S): OperatorFunction<T, S>',
+  'filter<T>(predicate: BooleanConstructor): OperatorFunction<T, TruthyTypesOf<T>>',
+  'filter<T>(predicate: (value: T, index: number) => boolean): MonoTypeOperatorFunction<T>'
+]
+</script>
 <template>
   <h4><code>filter</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
