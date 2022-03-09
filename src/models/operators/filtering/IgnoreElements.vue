f3345ff72bdde7180a3cce3902e469038e955e9f
howlwindy <howlwindy@gmail.com>
2022-03-09 21:24:23 +0800

    feat(operators): filtering
    ignoreElements

--- src/models/operators/filtering/IgnoreElements.vue
+++ src/models/operators/filtering/IgnoreElements.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['ignoreElements(): OperatorFunction<unknown, never>']
+</script>
 <template>
   <h4><code>ignoreElements</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
