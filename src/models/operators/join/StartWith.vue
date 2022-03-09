415c5a57e9e41c7c46550309ca880f9312d133f3
howlwindy <howlwindy@gmail.com>
2022-03-09 20:54:56 +0800

    feat(operators): join
    startWith

--- src/models/operators/join/StartWith.vue
+++ src/models/operators/join/StartWith.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = [
+  'startWith<T>(value: null): OperatorFunction<T, T | null>',
+  'startWith<T>(value: undefined): OperatorFunction<T, T | undefined>',
+  'startWith<T, A extends readonly unknown[] = T[]>(...values: A): OperatorFunction<T, T | ValueFromArray<A>>'
+]
+</script>
 <template>
   <h4><code>startWith</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
