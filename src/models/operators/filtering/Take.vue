d6e2b4530cfc746c276ab8e2fc32b418f51f0599
howlwindy <howlwindy@gmail.com>
2022-03-09 21:15:30 +0800

    feat(operators): filtering
    take

--- src/models/operators/filtering/Take.vue
+++ src/models/operators/filtering/Take.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SrcVue from '@/components/Src.vue'
+const src: string[] = ['take<T>(count: number): MonoTypeOperatorFunction<T>']
+</script>
 <template>
   <h4><code>take</code></h4>
+  <SrcVue :data="src"></SrcVue>
 </template>
 <style scoped></style>
