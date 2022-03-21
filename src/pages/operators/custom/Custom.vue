ecf6331b74b2fde78c931e436662a309c10e90c6
howlwindy <howlwindy@gmail.com>
2022-03-21 22:30:43 +0800

    feat(custom): practice

--- src/pages/operators/custom/Custom.vue
+++ src/pages/operators/custom/Custom.vue
@@ -1,5 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Subject } from 'rxjs'
+import {
+  // buffer,
+  // count,
+  // max,
+  // take,
+  // first,
+  // last,
+  // map,
+  // mapTo,
+  // filter,
+  // skip,
+  // single,
+  find
+} from 'rxjs/operators'
+import { ref, watch } from 'vue'
+const value = ref<string>('')
+const result = ref<any>(value.value)
+const subject = new Subject<string>()
+subject.pipe(find((v) => v.includes('a'))).subscribe({
+  next: (v) => {
+    console.log(v)
+    result.value = v
+  }
+})
+watch(value, (v) => {
+  subject.next(v)
+})
+</script>
 <template>
-  <h3>custom</h3>
+  <h4><code>custom</code></h4>
+  <input v-model="value" type="text" />
+  <p>{{ result }}</p>
 </template>
 <style scoped></style>
